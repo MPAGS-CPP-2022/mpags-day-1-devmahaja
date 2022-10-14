36df77a96c92e6bffbe9dbf9d76b9420ea926441
Vedanshu Mahajan <vmahajan@cern.ch>
2022-10-14 15:02:37 +0000

    End of today's session

---
+++ argumentreader.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    const std::vector<std::string> cmdLineArgs { argv, argv+argc }; 
+
+    for (argc i = 0; i < count; i++)
+    {
+        std::cout << cmdLineArgs[i] << std::endl;
+    }
+    
+
+}
--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 int main()
 {
     //comments be liek
-    std::cout << "Hello World!\n";
+    /*Is this visions of
+    multiline comment?*/
+    
+    char in_char('x');
+    std::string out_str("");
+
+    while(std::cin >> in_char)
+    {
+        if (std::isalpha(in_char))
+        {
+            out_str += std::toupper(in_char);
+            continue;
+        }
+        switch (in_char)
+        {
+        case '0':
+            out_str += "ZERO";
+            break;
+        case '1':
+            out_str += "ONE";
+            break;
+        case '2':
+            out_str += "TWO";
+            break;
+        case '3':
+            out_str += "THREE";
+            break;
+        case '4':
+            out_str += "FOUR";
+            break;
+        case '5':
+            out_str += "FIVE";
+            break;
+        case '6':
+            out_str += "SIX";
+            break;
+        case '7':
+            out_str += "SEVEN";
+            break;
+        case '8':
+            out_str += "EIGHT";
+            break;
+        case '9':
+            out_str += "NINE";
+            break;
+        
+        default:
+            break;
+        }
+
+    }
+    std::cout << out_str << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    const std::vector<std::string> cmdLineArgs { argv, argv+argc }; 
 }
